7a3d94e1acba680d9d6d678949713be0cb53d470
Chris <atnl@protonmail.com>
2021-04-26 15:42:16 -0400

    Create scammerspammer.py

---
+++ scammerspammer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import requests
+import threading
+
+url = ''
+
+data = {
+
+    'Email': '',
+
+    'Password': '',
+}
+
+def do_request():
+    while True:
+        response = requests.post(url, data=data).text
+        print(response)
+
+threads = []
+
+for i in range(50):
+        t = threading.Thread(target=do_request)
+        t.daemon = True
+        threads.append(t)
+
+for i in range(50):
+    threads[i].start()
+
+for i in range(50):
+    threads[i].join()
+
